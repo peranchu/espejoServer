a6af49b2ed13fe4d8e8a46e8f7511778ce29e94b
peranchu <n0rinogarcia@gmail.com>
2021-04-22 12:12:11 +0200

    gauges

--- public/js/socket.js
+++ public/js/socket.js
@@ -17,6 +17,8 @@ socket.addEventListener("message", e =>{
 });
 ///////////// FIN SOCKET ////////
 
+
+
 //// GAUGES ///////
 var opts = {
     angle: -0.24, // The span of the gauge arc
@@ -27,6 +29,11 @@ var opts = {
       strokeWidth: 0.035, // The thickness
       color: '#000000' // Fill color
     },
+    staticZones: [
+        {strokeStyle: "#30B32D", min: 0, max: 1200}, // Green
+        {strokeStyle: "#FFDD00", min: 1200, max: 2000}, // Yellow
+        {strokeStyle: "#F03E3E", min: 2000, max: 3000}  // Red
+    ],
     limitMax: false,     // If false, max value increases automatically if value > maxValue
     limitMin: false,     // If true, the min value of the gauge will be fixed
     colorStart: '#6FADCF',   // Colors
@@ -36,6 +43,8 @@ var opts = {
     highDpiSupport: true,     // High resolution support
 };
 
+
+
 //ANGRY
 var targetAngry = document.getElementById('angry'); 
 var gaugeAngry = new Gauge(targetAngry).setOptions(opts);
@@ -44,10 +53,50 @@ gaugeAngry.setMinValue(0);  // Prefer setter over gauge.minValue = 0
 gaugeAngry.animationSpeed = 32; // set animation speed (32 is default value)
 gaugeAngry.set(1250); 
 
-//DISGUSTED
+ //DISGUSTED
 var targetDisgusted = document.getElementById('disgusted'); 
 var gaugeDisgusted = new Gauge(targetDisgusted).setOptions(opts);
 gaugeDisgusted.maxValue = 3000; // set max gauge value
 gaugeDisgusted.setMinValue(0);  // Prefer setter over gauge.minValue = 0
 gaugeDisgusted.animationSpeed = 32; // set animation speed (32 is default value)
 gaugeDisgusted.set(300);
+
+//FEARFUL
+var targetFearful = document.getElementById('fearful'); 
+var gaugeFearful = new Gauge(targetFearful).setOptions(opts);
+gaugeFearful.maxValue = 3000; // set max gauge value
+gaugeFearful.setMinValue(0);  // Prefer setter over gauge.minValue = 0
+gaugeFearful.animationSpeed = 32; // set animation speed (32 is default value)
+gaugeFearful.set(500);
+ 
+//HAPPY
+var targetHappy = document.getElementById('happy'); 
+var gaugeHappy = new Gauge(targetHappy).setOptions(opts);
+gaugeHappy.maxValue = 3000; // set max gauge value
+gaugeHappy.setMinValue(0);  // Prefer setter over gauge.minValue = 0
+gaugeHappy.animationSpeed = 32; // set animation speed (32 is default value)
+gaugeHappy.set(500);
+
+//NEUTRAL
+var targetNeutral = document.getElementById('neutral'); 
+var gaugeNeutral = new Gauge(targetNeutral).setOptions(opts);
+gaugeNeutral.maxValue = 3000; // set max gauge value
+gaugeNeutral.setMinValue(0);  // Prefer setter over gauge.minValue = 0
+gaugeNeutral.animationSpeed = 32; // set animation speed (32 is default value)
+gaugeNeutral.set(2000);
+
+//SAD
+var targetSad = document.getElementById('sad'); 
+var gaugeSad = new Gauge(targetSad).setOptions(opts);
+gaugeSad.maxValue = 3000; // set max gauge value
+gaugeSad.setMinValue(0);  // Prefer setter over gauge.minValue = 0
+gaugeSad.animationSpeed = 32; // set animation speed (32 is default value)
+gaugeSad.set(2000);
+
+//SURPRISED
+var targetSurprised = document.getElementById('surprised'); 
+var gaugeSurprised = new Gauge(targetSurprised).setOptions(opts);
+gaugeSurprised.maxValue = 3000; // set max gauge value
+gaugeSurprised.setMinValue(0);  // Prefer setter over gauge.minValue = 0
+gaugeSurprised.animationSpeed = 32; // set animation speed (32 is default value)
+gaugeSurprised.set(2000);
